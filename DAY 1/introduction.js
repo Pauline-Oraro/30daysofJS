b86d56be204b9c1373ac7dca65fb9cab77bd1ca1
Pauline-Oraro <hiltrapauline@gmail.com>
2024-01-01 12:23:44 +0300

    into to JS

---
+++ DAY 1/introduction.js
@@ -0,0 +1,47 @@
+console.log("Hello World!", "Happy 2024!");
+console.log("Welcome to 30 days of Javascript!");
+
+
+
+//comments make the code more readable
+
+//This is a single line comment
+
+/* this is a multiline comment
+    multiline comments can take multiple lines
+*/ 
+
+//intro to data types
+//numbers
+let num1 = 3;
+console.log(num1);
+let num2 = -6;
+console.log(num2);
+let num3 = 4.70;
+console.log(num3);
+
+//strings
+let stringOne = "I am a Javascript developer";
+console.log(stringOne);
+let stringTwo = 'I love coding';
+console.log(stringTwo);
+
+//boolean
+true //value is true
+false //value is false
+
+//undefined; if we don't assign a value to a variable
+let myValue;
+console.log(myValue);
+
+//null means an empty value
+let emptyValue = null;
+console.log(emptyValue);
+
+//variables
+let firstName = 'Pauline';
+let lastName = "Oraro";
+let age = 22;
+let country = "Kenya";
+let isMarried = false;
+console.log(firstName, lastName, age, country, isMarried);
