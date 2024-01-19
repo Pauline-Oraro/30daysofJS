d59bf1890758947a5f0162987fad64f4dee07ecf
Pauline-Oraro <hiltrapauline@gmail.com>
2024-01-19 21:49:05 +0300

    Data types

--- DAY 2/dataTypes.js
+++ DAY 2/dataTypes.js
@@ -2,14 +2,34 @@
 
 //primitive data types. Once a primitive data is created we cannot modify it
 let myFirstName = "Pauline"//string
+let challenge = "30 Days of Javascript"
 let myAge = 22;//number
 let isMarried = false //boolean
 let value = null //null
 let country; //undefined
-console.log(myFirstName, myAge, isMarried, value, country);
+console.log(myFirstName, myAge, isMarried, value, country, challenge);
+console.log(challenge.length);
+console.log(myFirstName.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.includes("script"));
+let myString = challenge.split(" ");
+console.log(myString);
+console.log(challenge.replace("Javascript", "Python"));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt("J"));
+console.log(challenge.indexOf("a"));
+console.log(challenge.lastIndexOf("a"));
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.lastIndexOf("because"));
+console.log(sentence.trim());
+
+
 
 //non-primitive data types. Can be modified after being created
 let myArray = [10, 20, "Oraro"];
+let companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+let myCompanies = companies.join(" , ");
+console.log(myCompanies);
 let user = {
     name:"Kate",
     age: 23,
