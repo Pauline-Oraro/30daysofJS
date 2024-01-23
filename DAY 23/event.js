557fcb1cccd84ea067c9ac053a314aac732be3db
Pauline-Oraro <hiltrapauline@gmail.com>
2024-01-23 11:55:05 +0300

    Event Listeners

---
+++ DAY 23/event.js
@@ -0,0 +1,26 @@
+//event listeners.
+//We use addEventListener() method to listen to different event types on HTML elements.
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+    const newElement = document.createElement("p");
+    const newText = document.createTextNode("You have clicked the button");
+    newElement.appendChild(newText);
+    document.body.appendChild(newElement);
+});
+
+let paragraph = document.getElementById("myPara");
+paragraph.addEventListener("click", function(){
+    paragraph.style.backgroundColor = "crimson";
+    paragraph.style.fontFamily = "monospace";
+    paragraph.style.fontSize = "30px";
+});
+
+function clickMe(){ 
+    alert("Hi there");
+}
+
+let double = document.getElementById("double");
+double.addEventListener("dblclick", function(){
+    double.style.backgroundColor = "hotpink"
+})
