bee017e4e09c5d821c1341dc6bd79d1bd9c87670
Pauline-Oraro <hiltrapauline@gmail.com>
2024-01-27 21:45:28 +0300

    Project - Digital clock

---
+++ DAY 27/clock.js
@@ -0,0 +1,27 @@
+function digitalClock(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var session = "AM";
+
+    if(hour == 0){
+        hour = 12
+    }
+
+    if(hour > 12){
+        hour = hour -12;
+        session = "PM"
+    }
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    var time = hour + ":" + minutes + ":" + seconds + " " + session;
+    document.getElementById("clock").innerHTML = time;
+
+    setTimeout(digitalClock, 1000);
+}
+
+digitalClock();
